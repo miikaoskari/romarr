bf14767ca39448f7403625fd7df65a7f8520e25f
miika <miika.karjalainen13@gmail.com>
2023-12-17 19:20:44 +0200

    Work on navbar again

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import './navbar.css'
-import logo from '../../assets/png/logo-no-background.png'
+import logo from '../../assets/png/romarr-high-resolution-logo-black-transparent.png'
 const Navbar = () => {
     return (
         <div className="romarr__navbar">
             <div className="romarr__navbar-logo">
                 <img src={logo} alt="logo"/>
-
+            </div>
+            <div className="romarr__navbar-menu">
+                <a>Dashboard</a>
+                <a>Games</a>
+                <a>Activity</a>
+                <a>Settings</a>
+                <a>About</a>
             </div>
             <div className="romarr__navbar-search">
 
             </div>
-
-            <h1>games</h1>
-            <h1>activity</h1>
-            <h1>settings</h1>
-            <h1>about</h1>
+            <div className="romarr__navbar-user">
+                <button>User</button>
+            </div>
         </div>
     );
 };
