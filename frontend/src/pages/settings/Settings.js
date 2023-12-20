6f758f18b1d963c35a616bafb6742ab18e5ce562
miika <miika.karjalainen13@gmail.com>
2023-12-21 00:01:28 +0200

    fix h1 tag to match styling

--- frontend/src/pages/settings/Settings.js
+++ frontend/src/pages/settings/Settings.js
@@ -4,8 +4,10 @@ import React from "react";
 const Settings = () => {
     return (
         <div>
-            <div className={"flex flex-col py-3"}>
-                <h1 className={"text-3xl font-bold place-items-start mx-40"}>Indexers</h1>
+            <div className="flex flex-col py-3">
+            <h1 className={"text-3xl font-bold place-items-start mx-40"}>Indexers</h1>
+            </div>
+            <div className={""}>
                 <Indexers></Indexers>
             </div>
         </div>
