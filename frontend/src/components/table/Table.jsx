d7520a88cc48ce88c73728591a3c9cf56e872e07
miika <miika.karjalainen13@gmail.com>
2024-01-10 23:05:34 +0200

    Rework table design

--- frontend/src/components/table/Table.jsx
+++ frontend/src/components/table/Table.jsx
@@ -1,40 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Table = () => {
-    return (
-        <div>
-            <div className={"relative overflow-x-auto sm:rounded-lg mx-36 flex flex-col items-center bg-white rounded-2xl shadow-2xl border-2"}>
-                <table className={"w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400"}>
-                    <thead className={"text-xs text-gray-700 uppercase bg-gray-50"}>
-                    <tr>
-                        <th className="px-2 py-3">Song</th>
-                        <th className="px-2 py-3">Artist</th>
-                        <th className="px-2 py-3">Year</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr className={"odd:bg-white even:bg-gray-50 even:dark:bg-gray-800 border-b"}>
-                        <td className={"px-2 py-4 font-medium text-gray-900 whitespace-nowrap"}>The Sliding Mr. Bones
-                            (Next Stop, Pottersville)
-                        </td>
-                        <td className={"px-2 py-4"}>Malcolm Lockyer</td>
-                        <td className={"px-2 py-4"}>1961</td>
-                    </tr>
-                    <tr className={"odd:bg-white even:bg-gray-50 border-b"}>
-                        <td className={"px-2 py-4 font-medium text-gray-900 whitespace-nowrap"}>Witchy Woman</td>
-                        <td className={"px-2 py-4"}>The Eagles</td>
-                        <td className={"px-2 py-4"}>1972</td>
-                    </tr>
-                    <tr className={"odd:bg-white even:bg-gray-50 border-b"}>
-                        <td className={"px-2 py-4 font-medium text-gray-900 whitespace-nowrap"}>Shining Star</td>
-                        <td className={"px-2 py-4"}>Earth, Wind, and Fire</td>
-                        <td className={"px-2 py-4"}>1975</td>
-                    </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    );
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/games')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
+  return (
+    <table className={'w-full text-left text-sm text-gray-500 rtl:text-right dark:text-gray-400'}>
+      <thead className={'bg-gray-50 uppercase text-black'}>
+        <tr>
+          <th className="px-2 py-3">Title</th>
+          <th className="px-2 py-3">Source Title</th>
+          <th className="px-2 py-3">Console</th>
+          <th className="px-2 py-3">Date</th>
+          <th className={'px-2 py-3'}></th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((item, index) => (
+          <tr key={index} className={'border-b odd:bg-white even:bg-gray-50 even:dark:bg-gray-800'}>
+            <td className={'whitespace-nowrap px-2 py-4 font-medium text-black'}>{item.title}</td>
+            <td className={'px-2 py-4'}>{item.sourceTitle}</td>
+            <td className={'px-2 py-4'}>{item.console}</td>
+            <td className={'px-2 py-4'}>{item.date}</td>
+            <td className={'px-2 py-4 text-right'}>
+              <FontAwesomeIcon icon={faTrash} />
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default Table;
