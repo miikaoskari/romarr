eec15b8fcb3f6c7e25e5625c1967f557e017a066
miika <miika.karjalainen13@gmail.com>
2024-01-13 21:41:42 +0100

    Add image implementation for Card

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const Card = ({ children }) => {
+const Card = ({ children, image }) => {
   return (
     <div className="max-h-md auto mx-2 my-2 max-w-md rounded-lg border-2 border-gray-50 bg-white p-6 shadow-md">
       {children}
+      {image && <img src={image} alt="Game"/>}
     </div>
   );
 };
 
 Card.propTypes = {
   children: propTypes.node,
+  image: propTypes.string,
 };
 
 export default Card;
--- frontend/src/pages/games/Games.js
+++ frontend/src/pages/games/Games.js
@@ -1,12 +1,34 @@
 import Grid from '../../components/grid/Grid';
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Card } from '../../components';
 const Games = () => {
+  const [image, setImage] = React.useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/api/v1/games')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => setImage(data))
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <div>
       <div className={'flex flex-col py-3'}>
         <h1 className={'mx-6 place-items-start text-3xl font-bold'}>Games</h1>
       </div>
-      <Grid></Grid>
+
+      <Grid>
+        {image.map((image) => (
+          <Card key={image.id} image={image} />
+        ))}
+      </Grid>
     </div>
   );
 };
