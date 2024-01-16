84bb0a770c5fd25bdfdf9bf266adac4266bd2a85
miika <miika.karjalainen13@gmail.com>
2024-01-16 23:52:51 +0200

    Modify card with more options

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
-const Card = ({ children, image }) => {
+const Card = ({ children, image, header, content }) => {
   return (
     <div className="max-h-md auto mx-2 my-2 max-w-md rounded-lg border-2 border-gray-50 bg-white p-6 shadow-md">
       {children}
       {image && <img src={image} alt="Game"/>}
+      {header && <h1 className="text-2xl font-bold">{header}</h1>}
+      {content && <p className="text-gray-700 text-base">{content}</p>}
     </div>
   );
 };
@@ -13,6 +15,8 @@ const Card = ({ children, image }) => {
 Card.propTypes = {
   children: propTypes.node,
   image: propTypes.string,
+  header: propTypes.string,
+  content: propTypes.string,
 };
 
 export default Card;
