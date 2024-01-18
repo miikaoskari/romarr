a966827724b75772c1f66fc76ac2fe4f2ed5bcf5
miika <miika.karjalainen13@gmail.com>
2024-01-19 00:31:44 +0200

    Add buttons with modal to settings page

--- frontend/src/pages/settings/Settings.js
+++ frontend/src/pages/settings/Settings.js
@@ -1,7 +1,18 @@
-import React from 'react';
-import { Card, Grid } from '../../components';
+import React, { Fragment, useState } from 'react';
+import { Button, Card, Grid } from '../../components';
+import { Dialog, Transition } from '@headlessui/react';
 
 const Settings = () => {
+  let [isOpen, setIsOpen] = useState(false);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
   const [indexers, setIndexers] = React.useState([]);
   const [clients, setClients] = React.useState([]);
 
@@ -51,6 +62,9 @@ const Settings = () => {
           </div>
         )}
       </div>
+      <div className={'px-6 py-4'}>
+        <Button title={'Add'} onClick={openModal}></Button>
+      </div>
       <h1 className={'mx-6 place-items-start text-3xl font-bold'}>Download clients</h1>
       <div className={''}>
         {clients.length > 0 ? (
@@ -65,6 +79,53 @@ const Settings = () => {
           </div>
         )}
       </div>
+      <div className={'px-6 py-4'}>
+        <Button title={'Add'} onClick={openModal}></Button>
+      </div>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
+                    Add Indexer
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <p className="text-sm text-gray-500">
+
+                    </p>
+                  </div>
+
+                  <div className="mt-4">
+                    <Button title={"Save"} onClick={closeModal}></Button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   );
 };
