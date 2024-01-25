2273c3c819ba9bb3724f001bf8c8a7cd42967d22
miika <miika.karjalainen13@gmail.com>
2024-01-25 23:49:21 +0200

    Remove remaining css file

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import './navbar.css';
 import logo from '../../assets/png/romarr-high-resolution-logo-black-transparent2.0.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-solid-svg-icons';
