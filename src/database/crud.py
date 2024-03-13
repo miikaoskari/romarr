aef416cadd13f817f2c929f8ff9c1178b6d6ab32
miika <miika.karjalainen13@gmail.com>
2024-03-13 21:19:15 +0200

    add crud commands

--- src/database/crud.py
+++ src/database/crud.py
@@ -2,12 +2,7 @@ from sqlalchemy.orm import Session
 
 from . import models, schemas
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.User).filter(models.User.id == user_id).first()
-
-def get_user_by_username(db: Session, username: str):
-    return db.query(models.User).filter(models.User.username == username).first()
-
+# CREATE
 def create_user(db: Session, user: schemas.UserCreate):
     fake_hashed_password = user.hashed_password + "notreallyhashed"
     db_user = models.User(email=user.username, hashed_password=fake_hashed_password)
@@ -15,3 +10,30 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+def create_game(db: Session, game: schemas.GameCreate):
+    db_game = models.Game(**game.model_dump())
+    db.add(db_game)
+    db.commit()
+    db.refresh(db_game)
+    return db_game
+
+# READ
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_games(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Game).offset(skip).limit(limit).all()
+
+def get_game_by_id(db: Session, game_id: int):
+    return db.query(models.Game).filter(models.Game.id == game_id).first()
+
+# UPDATE
+
+
+# DELETE
+
+
