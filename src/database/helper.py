d0223ba659a8a9031a780383dd00851c2b381bd7
miikaoskari <miika.karjalainen13@gmail.com>
2025-08-27 20:37:49 +0300

    Delete database actions

--- src/database/helper.py
+++
@@ -1,7 +0,0 @@
-def create_schema_instances(data, schema, attr):
-    instances = []
-    if data:
-        for id in data:
-            instance = schema(**{attr: id})
-            instances.append(instance)
-    return instances
